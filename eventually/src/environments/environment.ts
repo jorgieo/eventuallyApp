4fdb68fe2e46310383c818ad4277c1059b759897
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-11-09 20:35:22 -0500

    Added Firebase environment object

--- eventually/src/environments/environment.ts
+++ eventually/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCm9IwP2hy8HL3LAoaFjE3StQAjuQxpHm0",
+    authDomain: "eventuallyapp-5523e.firebaseapp.com",
+    databaseURL: "https://eventuallyapp-5523e.firebaseio.com",
+    projectId: "eventuallyapp-5523e",
+    storageBucket: "eventuallyapp-5523e.appspot.com",
+    messagingSenderId: "984022734172",
+    appId: "1:984022734172:web:b654a1ecbb8327d32c5d99"
+  }
 };
 
 /*
