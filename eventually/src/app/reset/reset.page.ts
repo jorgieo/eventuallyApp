24a4ea42a05b7454d13dd64b522a8d29c2e15454
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-11-13 13:59:02 -0500

    Scaffold of Reset page

--- eventually/src/app/reset/reset.page.ts
+++ eventually/src/app/reset/reset.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { LoadingController, NavController, ToastController } from '@ionic/angular';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-reset',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset.page.scss'],
 })
 export class ResetPage implements OnInit {
+  user  = {} as User;
 
-  constructor() { }
+  constructor(
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private afAuth: AngularFireAuth,
+    private navCtrl: NavController) {}
 
   ngOnInit() {
+  }
+
+  // TODO: method to send email to admin
+  async reset(user){
+    // Do something...
+    console.log(user)
+  }
+
+  formValidation() {
+    if (!this.user.email){
+      this.showToast("Enter Email!");
+      return false;
+    }
+    if (!this.user.password){
+      this.showToast("Enter Password!");
+      return false;
+    }
+
+    return true
+  }
+
+  showToast(message:string) {
+    this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    }).then(toastData => toastData.present());
   }
 
 }
