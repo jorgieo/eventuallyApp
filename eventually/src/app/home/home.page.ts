a370441c8a3de6824cd3e6ae2ce24280e80dc1f0
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-11-13 20:06:00 -0500

    Added form validation and toast controller

--- eventually/src/app/home/home.page.ts
+++ eventually/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { LoadingController, NavController, ToastController } from '@ionic/angular';
+import { Event } from '../../models/event.model'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  event = {} as Event;
 
-  constructor() {}
+  constructor(
+    private toastCtrl:ToastController,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController,
+    private firestore: AngularFirestore) {}
+
+    // get events
+  
+    formValidation() {
+      if (!this.event.title){
+        this.showToast("Enter Title!");
+        return false;
+      }
+      if (!this.event.datetime){
+        this.showToast("Select Date!");
+        return false;
+      }
+  
+      return true
+    }
+  
+    showToast(message:string) {
+      this.toastCtrl.create({
+        message: message,
+        duration: 3000
+      }).then(toastData => toastData.present());
+    }
 
 }
