163ca6824961505323b45e243a210b2087780743
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-11-18 21:22:45 -0500

    Experimenting with grids.

--- eventually/src/app/event-details/event-details.page.ts
+++ eventually/src/app/event-details/event-details.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-details.page.scss'],
 })
 export class EventDetailsPage implements OnInit {
+  items =[];
 
   constructor() { }
 
   ngOnInit() {
+    this.makeItems();
+  }
+
+  makeItems(){
+    for (let index = 0; index < 10; index++) {
+      this.items.push(index + 1);
+      
+    }
   }
 
 }
