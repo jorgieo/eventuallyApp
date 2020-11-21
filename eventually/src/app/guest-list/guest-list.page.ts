2a9ac79b7ea2c8f4af05a445fcdd07b6b13e2cb0
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-11-21 18:59:19 -0500

    Refactored guest avatar as exportable object

--- eventually/src/app/guest-list/guest-list.page.ts
+++ eventually/src/app/guest-list/guest-list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { avatars } from '../../assets/avatars'
 
 @Component({
   selector: 'app-guest-list',
@@ -8,6 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GuestListPage implements OnInit {
   eventid = this.route.snapshot.params.eventid;
+  avatar = avatars.male_avatar;
 
   constructor(private route: ActivatedRoute) { }
 
---
+++ eventually/src/assets/avatars.ts
@@ -0,0 +1,4 @@
+export const avatars = {
+    male_avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShortFlat&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=Blank&clotheType=Hoodie&clotheColor=Black&eyeType=Happy&eyebrowType=Default&mouthType=Default&skinColor=Brown',
+    female_avatar: 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairCurvy&accessoriesType=Prescription01&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Happy&eyebrowType=Default&mouthType=Default&skinColor=Light'
+}
