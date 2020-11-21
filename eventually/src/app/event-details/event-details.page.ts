21635508166a146b7e64aae4d4da55d74e654772
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-11-21 14:09:45 -0500

    Retrieving event data from database

--- eventually/src/app/event-details/event-details.page.ts
+++ eventually/src/app/event-details/event-details.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { Event } from 'src/models/event.model';
 
 @Component({
   selector: 'app-event-details',
@@ -7,20 +10,36 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./event-details.page.scss'],
 })
 export class EventDetailsPage implements OnInit {
-  items =[];
+  event = {} as Event;
+  eventid = this.route.snapshot.params.eventid;
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(
+    private route:ActivatedRoute,
+    private firestore: AngularFirestore,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController) { }
 
   ngOnInit() {
-    this.makeItems();
     console.log(this.route.snapshot.params);
+    this.getEventById(this.eventid);
   }
 
-  makeItems(){
-    for (let index = 0; index < 10; index++) {
-      this.items.push(index + 1);
-      
-    }
+  async getEventById(eventid:string){
+    let loader = this.loadingCtrl.create({
+      message: "Please wait..."
+    });
+    (await loader).present();
+
+    this.firestore.doc('events/' + eventid).valueChanges().subscribe(data => {
+      this.event.title = data['title'];
+      this.event.details = data['details'];
+      this.event.venue = data['venue'];
+      this.event.date = data['date'];
+      this.event.time = data['time'];
+      console.log(data);
+    });
+    
+    (await loader).dismiss();
   }
 
 }
