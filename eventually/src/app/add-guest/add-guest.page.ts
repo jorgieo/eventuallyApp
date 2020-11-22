afeca1224bc3e530ab13c0b4c8cdcff1c6215185
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-11-21 20:56:20 -0500

    Created form to add guests. Closes #16

--- eventually/src/app/add-guest/add-guest.page.ts
+++ eventually/src/app/add-guest/add-guest.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { LoadingController, NavController, ToastController } from '@ionic/angular';
+import { Guest } from 'src/models/guest.model';
 
 @Component({
   selector: 'app-add-guest',
@@ -6,10 +10,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-guest.page.scss'],
 })
 export class AddGuestPage implements OnInit {
+  guest = {} as Guest;
+  uid = this.route.snapshot.params.uid;
+  eventid = this.route.snapshot.params.eventid;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private loadingCtrl: LoadingController,
+    private navCtrl: NavController,
+    private firestore: AngularFirestore,
+    private toastCtrl:ToastController) { }
 
   ngOnInit() {
+  }
+
+  selectedGender(event){
+    this.guest.gender = event.detail.value;
+  }
+
+  selectedRSVP(event){
+    this.guest.response = event.detail.value;
+  }
+
+  async addGuest(){
+    if(!this.guest.gender){
+      this.guest.gender = 'n';
+    }
+    if(!this.guest.response){
+      this.guest.response = 'w';
+    }
+    
+    if(this.formValidation()){
+      let loader = this.loadingCtrl.create({
+        message: "Please wait..."
+      });
+      (await loader).present();
+
+      this.guest.eventid = this.eventid;
+      this.guest.uid = this.uid;
+
+      try {
+        await this.firestore.collection('guests').add(this.guest);
+
+      } catch (e) {
+        console.log(e)
+        this.showToast(e);
+      }
+
+      (await loader).dismiss();
+
+      this.navCtrl.navigateBack(['home',this.uid,'event-details',this.eventid,'guest-list']);
+    }
+  }
+
+  formValidation() {
+    if (!this.guest.name){
+      this.showToast("Enter Guest Name!");
+      return false;
+    }
+    if (!this.guest.email){
+      this.showToast("Enter Guest Email!");
+      return false;
+    }
+    return true
+  }
+
+  showToast(message:string) {
+    this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    }).then(toastData => toastData.present());
   }
 
 }
