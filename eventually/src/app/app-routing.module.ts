afe7778fba6ee69e4b7210edfd1d0492fb3091f2
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-11-21 19:15:48 -0500

    Updated page route

--- eventually/src/app/app-routing.module.ts
+++ eventually/src/app/app-routing.module.ts
@@ -38,12 +38,13 @@ const routes: Routes = [
   {
     path: 'home/:uid/event-details/:eventid/guest-list',
     loadChildren: () => import('./guest-list/guest-list.module').then( m => m.GuestListPageModule)
-  },  {
+  },
+  {
     path: 'guest-details',
     loadChildren: () => import('./guest-details/guest-details.module').then( m => m.GuestDetailsPageModule)
   },
   {
-    path: 'add-guest',
+    path: 'home/:uid/event-details/:eventid/guest-list/add-guest',
     loadChildren: () => import('./add-guest/add-guest.module').then( m => m.AddGuestPageModule)
   },
 
