23e9df03daea3c30726a32bc8d8020460948c0db
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-12-04 21:32:04 -0500

    Injected route guard to pertinent pages

--- eventually/src/app/app-routing.module.ts
+++ eventually/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path: 'home/:uid',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: '',
@@ -25,11 +27,13 @@ const routes: Routes = [
   },
   {
     path: 'home/:uid/event-details/:eventid',
-    loadChildren: () => import('./event-details/event-details.module').then( m => m.EventDetailsPageModule)
+    loadChildren: () => import('./event-details/event-details.module').then( m => m.EventDetailsPageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: 'home/:uid/add-event',
-    loadChildren: () => import('./add-event/add-event.module').then( m => m.AddEventPageModule)
+    loadChildren: () => import('./add-event/add-event.module').then( m => m.AddEventPageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: 'edit-event',
@@ -37,15 +41,18 @@ const routes: Routes = [
   },
   {
     path: 'home/:uid/event-details/:eventid/guest-list',
-    loadChildren: () => import('./guest-list/guest-list.module').then( m => m.GuestListPageModule)
+    loadChildren: () => import('./guest-list/guest-list.module').then( m => m.GuestListPageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: 'home/:uid/event-details/:eventid/guest-list/:guestid/guest-details',
-    loadChildren: () => import('./guest-details/guest-details.module').then( m => m.GuestDetailsPageModule)
+    loadChildren: () => import('./guest-details/guest-details.module').then( m => m.GuestDetailsPageModule),
+    canLoad: [AuthGuard]
   },
   {
     path: 'home/:uid/event-details/:eventid/guest-list/add-guest',
-    loadChildren: () => import('./add-guest/add-guest.module').then( m => m.AddGuestPageModule)
+    loadChildren: () => import('./add-guest/add-guest.module').then( m => m.AddGuestPageModule),
+    canLoad: [AuthGuard]
   },
 
 
