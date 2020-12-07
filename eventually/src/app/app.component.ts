d7290b047528d2d418c0fd131a66a1a28a65ab93
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-12-06 21:30:24 -0500

    Converted to PWA

--- eventually/src/app/app.component.ts
+++ eventually/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private swUpdate: SwUpdate
   ) {
     this.initializeApp();
   }
@@ -23,5 +25,11 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    if(this.swUpdate.available){
+      this.swUpdate.available.subscribe(() => {
+        if(confirm('A new version is available. Load it?'))
+        window.location.reload();
+      })
+    }
   }
 }
